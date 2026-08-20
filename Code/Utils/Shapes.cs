c6fe85701a8a987f3dd723b4f29703880e1ce939
JedynyCywilizowany <marek_b14@o2.pl>
2026-08-20 16:42:42 +0200

    Decreased cached circle memory usage

--- Code/Utils/Shapes.cs
+++ Code/Utils/Shapes.cs
@@ -5,38 +5,43 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.Xna.Framework;
 
+using SmallPoint=(sbyte x,sbyte y);
+
 namespace ColonyLib;
 
 partial class ColonyUtils
 {
-	private static readonly List<ImmutableArray<Point>?> circleCache=[[new(0,0)]];
+	private static readonly List<ImmutableArray<SmallPoint>?> circleCache=[[new(0,0)]];
 	private static void ChacheCircle(int radius)
 	{
-		if ((uint)radius>byte.MaxValue) throw new ArgumentOutOfRangeException(nameof(radius));
+		if ((uint)radius>(uint)sbyte.MaxValue) throw new ArgumentOutOfRangeException(nameof(radius));
 
 		if (circleCache.Count<=radius) CollectionsMarshal.SetCount(circleCache,radius+1);
 		if (!circleCache[radius].HasValue)
 		{
-			List<Point> points=new();
+			List<SmallPoint> points=new();
 			var radiusSQ=radius*radius;
 			for (int y=-radius;y<=radius;y++) for (int x=-radius;x<=radius;x++)
 			{
-				if ((x*x)+(y*y)<=radiusSQ) points.Add(new(x,y));
+				if ((x*x)+(y*y)<=radiusSQ) points.Add(((sbyte)x,(sbyte)y));
 			}
-			circleCache[radius]=points.OrderBy((point)=>point.ManhattanDistance(Point.Zero)).ToImmutableArray();
+			circleCache[radius]=points.OrderBy((point)=>Math.Abs((int)point.x)+Math.Abs((int)point.y)).ToImmutableArray();
 		}
 	}
 	/// <summary>
 	/// Used to get a perfect circle out of tiles in high-performance scenarios.<br/>
-	/// Radius must be within the range of <see cref="byte"/>.<br/>
+	/// For technical reasons, radius must be within the range 0 - 127.<br/>
 	/// Radius 0 is a single tile.<br/>
 	/// <br/>
-	/// The returned array is to be enumerated, it contains the offset from center for every tile in the circle.<br/>
+	/// The returned collection is to be enumerated, it contains the offset from center for every tile in the circle.<br/>
 	/// Increases performance by reusing generated circle-arrays for every call with the same <paramref name="radius"/> parameter.
 	/// </summary>
-	public static ImmutableArray<Point> GetCachedCircle(int radius)
+	public static IEnumerable<Point> GetCachedCircle(int radius)
 	{
 		ChacheCircle(radius);
-		return circleCache[radius]!.Value;
+		foreach (var (x,y) in circleCache[radius]!.Value)
+		{
+			yield return new Point(x,y);
+		}
 	}
 }
